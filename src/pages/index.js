c60d8436306c13dd02ff0f7637549ba0eda38cbe
Chase McCoy <chasem000@gmail.com>
2019-06-15 20:38:07 -0500

    group lists by type

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,36 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 const IndexPage = ({ data: { airtable } }) => (
-  <ul>
-    {airtable.nodes.map(node => (
-      <li key={node.id}>
-        <a href={node.data.url} title={node.data.title}>{node.data.title}</a>
-      </li>
+  <React.Fragment>
+    {airtable.groups.map(group => (
+      <React.Fragment key={group.type}>
+        <h2>{group.type}</h2>
+
+        <ul>
+          {group.nodes.map(node => (
+            <li key={node.id}>
+              <a href={node.data.url} title={node.data.title}>{node.data.title}</a>
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
     ))}
-  </ul>
+  </React.Fragment>
 )
 
 export default IndexPage
 
 export const query = graphql`
   query IndexQuery {
-    airtable: allAirtable {
-      nodes {
-        id
-        data {
-          title: Title
-          url: URL
+    airtable: allAirtable(sort: {fields: data___Title, order: ASC}) {
+      groups: group(field: data___Type) {
+        type: fieldValue
+        nodes {
+          id
+          data {
+            title: Title
+            url: URL
+          }
         }
       }
     }
