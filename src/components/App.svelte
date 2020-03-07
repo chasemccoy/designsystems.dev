caac95280b8a550a2cd8747d7761f600e52f0121
Chase McCoy <chasem000@gmail.com>
2020-03-07 14:15:13 -0600

    clean up

---
+++ src/components/App.svelte
@@ -0,0 +1,22 @@
+<svelte:options tag="my-app" />
+
+<script>
+  let count = 1;
+
+  // the `$:` means 're-run whenever these values change'
+  $: doubled = count * 2;
+  $: quadrupled = doubled * 2;
+
+  const handleClick =() => {
+    count += 1;
+  }
+</script>
+
+<main>
+  <button on:click="{handleClick}">Count: {count}</button>
+
+  <hr />
+
+  <p>{count} * 2 = {doubled}</p>
+  <p>{doubled} * 2 = {quadrupled}</p>
+</main>
