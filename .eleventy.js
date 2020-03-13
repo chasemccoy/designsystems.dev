ed0599657fc9104a0be05f0b02f8ad0358528f15
Chase McCoy <chasem000@gmail.com>
2020-03-13 18:23:50 -0500

    setup table of contents

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require('markdown-it');
+let markdownItAnchor = require('markdown-it-anchor');
+const pluginTOC = require('eleventy-plugin-nesting-toc');
 
 module.exports = config => {
   config.addPlugin(syntaxHighlight);
+  config.addPlugin(pluginTOC);
 
   config.addPassthroughCopy({
     'src/_includes/css/*': 'css',
@@ -19,13 +23,22 @@ module.exports = config => {
     return [...collection.getFilteredByGlob('./docs/*.md')];
   });
 
+  const options = {
+    html: true,
+    // Autoconvert URL-like text to links
+    linkify: true,
+    typographer: true,
+  };
+  const markdownLib = markdownIt(options).use(markdownItAnchor);
+  config.setLibrary('md', markdownLib);
+
   return {
     dir: {
       output: 'dist',
       includes: 'src/_includes',
       data: 'src/_data',
     },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
   };
 };
--- docs/design-tokens.md
+++ docs/design-tokens.md
@@ -2,4 +2,10 @@
 title: Design tokens
 ---
 
-test
+## Recommended reading 
+
+- designtokens.org
+- [CSS Tricks: What Are Design Tokens?](https://css-tricks.com/what-are-design-tokens/)
+- [Nathan Curtis: Tokens in Design Systems](https://medium.com/eightshapes-llc/tokens-in-design-systems-25dd82d58421)
+
+### Test
